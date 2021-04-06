66374ce87dea27305e5c5e1ecde1025eecede9b7
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-04-06 13:48:12 -0600

    Called the methods in the service

--- ClientWCF/Controllers/DemoController.cs
+++ ClientWCF/Controllers/DemoController.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using DemoServiceReference;
 
 namespace ClientWCF.Controllers
 {
     [Route("demo")]
     public class DemoController : Controller
     {
+        private DemoServiceClient _demoServiceClient = new DemoServiceClient();
+
         [Route("")]
         [Route("index")]
         [Route("~/")]
         public IActionResult Index()
         {
+            ViewBag.result1 = _demoServiceClient.HelloAsync().Result;
+            ViewBag.result2 = _demoServiceClient.HiAsync("ABC").Result;
+            ViewBag.result3 = _demoServiceClient.SumAsync(12,23).Result;
             return View();
         }
     }
