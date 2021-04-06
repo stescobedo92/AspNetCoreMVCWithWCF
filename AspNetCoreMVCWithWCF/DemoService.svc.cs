508262fa267d50738e17bebfa52abaeb7f5a9518
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-04-06 13:15:40 -0600

    Was created the demo service

---
+++ AspNetCoreMVCWithWCF/DemoService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AspNetCoreMVCWithWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DemoService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DemoService.svc or DemoService.svc.cs at the Solution Explorer and start debugging.
+    public class DemoService : IDemoService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
