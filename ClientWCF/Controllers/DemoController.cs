1a9319a2872e300a1a9d9a840d236356d2424cf3
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-04-06 14:14:38 -0600

    Consume the proudct service

--- ClientWCF/Controllers/DemoController.cs
+++ ClientWCF/Controllers/DemoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DemoServiceReference;
+using ProductServiceReference;
 
 namespace ClientWCF.Controllers
 {
@@ -11,6 +12,7 @@ namespace ClientWCF.Controllers
     public class DemoController : Controller
     {
         private DemoServiceClient _demoServiceClient = new DemoServiceClient();
+        private ProductServiceClient _productServiceClient = new ProductServiceClient();
 
         [Route("")]
         [Route("index")]
@@ -20,6 +22,10 @@ namespace ClientWCF.Controllers
             ViewBag.result1 = _demoServiceClient.HelloAsync().Result;
             ViewBag.result2 = _demoServiceClient.HiAsync("ABC").Result;
             ViewBag.result3 = _demoServiceClient.SumAsync(12,23).Result;
+
+            //Get data of product service
+            ViewBag.product = _productServiceClient.findAsync().Result;
+            ViewBag.products = _productServiceClient.findAllAsync().Result;
             return View();
         }
     }
