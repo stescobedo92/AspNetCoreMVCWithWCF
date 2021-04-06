3a3fac23cf7605cc5bb3a6cb1d1ad0dafa6e0e4b
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-04-06 13:57:42 -0600

    Added the new ProductService

---
+++ AspNetCoreMVCWithWCF/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AspNetCoreMVCWithWCF
+{
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        Product finc();
+
+        [OperationContract]
+        List<Product> findAll();
+    }
+}
