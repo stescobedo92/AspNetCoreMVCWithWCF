eb2d3e52ba6fa8b81989b2aebc79ec3116ba62e9
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-04-06 14:01:38 -0600

    Implementation of ProductService

--- AspNetCoreMVCWithWCF/ProductService.svc.cs
+++ AspNetCoreMVCWithWCF/ProductService.svc.cs
@@ -7,12 +7,21 @@ using System.Text;
 
 namespace AspNetCoreMVCWithWCF
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select ProductService.svc or ProductService.svc.cs at the Solution Explorer and start debugging.
     public class ProductService : IProductService
     {
-        public void DoWork()
+        public Product find()
         {
+            return new Product { Id = "p01", Name = "Product 1", Price = 123 };
+        }
+
+        public List<Product> findAll()
+        {
+            return new List<Product>
+            {
+                new Product { Id = "p01", Name = "Product 1", Price = 123 };
+                new Product { Id = "p02", Name = "Product 2", Price = 124 };
+                new Product { Id = "p03", Name = "Product 3", Price = 125 };
+            }
         }
     }
 }
